170f668781a81c88a11f01862b3b9337fd5d0a99
Richard Joubert <richard@nsd.academy>
2022-07-26 17:52:46 -0400

    Added the Web Development Projects Directory

---
+++ Projects/Dreamweaver Sites/Simple Multi-Page Project 001/config.rb
@@ -0,0 +1,25 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "D:/Coding/Web Development/Projects/Dreamweaver Sites/Simple Multi-Page Project 001/html"
+css_dir = "assets/styles/css/css_output"
+sass_dir = ""
+images_dir = "assets/images/hosted_images"
+generated_images_path = "D:/Coding/Web Development/Projects/Dreamweaver Sites/Simple Multi-Page Project 001/assets/images/generaged_images"
+javascripts_dir = "javascripts"
+fonts_dir = "assets/fonts"
+
+output_style = :nested
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass  scss && rm -rf sass && mv scss sass
+sourcemap = true
